1aebb5e903df91881aac438b7f58e4b6381ae40e
Bismita Guha <bismita.guha@gmail.com>
2021-06-17 14:33:59 +0000

    Initial commit

---
+++ services/aws-serverless/update.js
@@ -0,0 +1,58 @@
+
+  
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.update = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+
+  // validation
+  if (typeof data.text !== 'string' || typeof data.checked !== 'boolean') {
+    console.error('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t update the todo item.',
+    });
+    return;
+  }
+
+  const params = {
+    TableName: "group12_table",
+    Key: {
+      id: event.pathParameters.id,
+    },
+    ExpressionAttributeValues: {
+      ':Price': data.Price,
+      ':Ticker_Symbol': data.Ticker_Symbol,
+      ':Company': data.Company,
+    },
+    UpdateExpression: 'SET Price = :Price, Ticker_Symbol = :Ticker_Symbol, Company = :Company',
+    ReturnValues: 'ALL_NEW',
+  };
+
+  // update the todo in the database
+  dynamoDb.update(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the todo item.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Attributes),
+    };
+    callback(null, response);
+  });
+};
